86a501fdd63e25e9356cb71a65e104ce186eebe9
dereck <dereck@dereckredes.teste>
2025-07-31 15:12:19 -0300

    so pra mudar de branch

--- src/components/UserScreen.jsx
+++ src/components/UserScreen.jsx
@@ -4,7 +4,7 @@ function UserScreen(){
     return(
         <div className="flex justify-center items-center w-screen h-screen bg-[#4B3C5D]">
             <div className="w-[95%] h-[95%] flex justify-center items-center space-y-7 p-9 rounded-xl bg-slate-700 drop-shadow-[0px_10px_20px_rgba(0,0,0,0.5)]">
-                <h1 className="text-6xl text-white font-semibold">Bem-vindo(a) {name}</h1>
+                
             </div>
         </div>
     )
